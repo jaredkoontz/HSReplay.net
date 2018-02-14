0d305e3af46f3475d85495a8c71c93073eef8ec0
Jerome Leclanche <jerome@leclan.ch>
2018-02-14 21:37:38 +0200

    Implement local mocking of AWS services using moto

--- hsreplaynet/utils/aws/clients.py
+++ hsreplaynet/utils/aws/clients.py
@@ -2,13 +2,34 @@ import boto3
 from django.conf import settings
 
 
-if settings.CONNECT_TO_AWS:
-	IAM = boto3.client("iam")
-	LAMBDA = boto3.client("lambda", region_name=settings.AWS_DEFAULT_REGION)
-	KINESIS = boto3.client("kinesis", region_name=settings.AWS_DEFAULT_REGION)
-	S3 = boto3.client("s3")
-	FIREHOSE = boto3.client("firehose", region_name=settings.AWS_DEFAULT_REGION)
-	SQS = boto3.client("sqs", region_name=settings.AWS_DEFAULT_REGION)
-else:
-	# Stubbed to prevent ImportErrors
-	IAM, LAMBDA, KINESIS, S3, FIREHOSE, SQS = None, None, None, None, None, None
+def get_clients():
+	return (
+		boto3.client("iam"),
+		boto3.client("firehose", region_name=settings.AWS_DEFAULT_REGION),
+		boto3.client("kinesis", region_name=settings.AWS_DEFAULT_REGION),
+		boto3.client("lambda", region_name=settings.AWS_DEFAULT_REGION),
+		boto3.client("s3"),
+		boto3.client("sqs", region_name=settings.AWS_DEFAULT_REGION),
+	)
+
+
+if not settings.CONNECT_TO_AWS:
+	import moto
+
+	# https://github.com/spulec/moto
+	# If we're in an environment where we don't connect to upstream AWS
+	# (such as local dev), the following mocks the AWS services we use.
+
+	mocks = (
+		moto.mock_iam,
+		moto.mock_kinesis,
+		moto.mock_lambda,
+		moto.mock_s3,
+		moto.mock_sqs
+	)
+
+	for mock in mocks:
+		get_clients = mock(get_clients)
+
+
+IAM, FIREHOSE, KINESIS, LAMBDA, S3, SQS = get_clients()
