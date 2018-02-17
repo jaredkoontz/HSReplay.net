3bc8725c52fea81bdb7806e0b411faf33079688a
Jerome Leclanche <jerome@leclan.ch>
2018-02-18 01:14:33 +0200

    Clean up wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-# flake8: noqa
 
-import os; os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hsreplaynet.settings")
-import sys; sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+import os
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hsreplaynet.settings")
 
 application = get_wsgi_application()
