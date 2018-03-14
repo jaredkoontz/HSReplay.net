9290ae001e406f5c426cd0ba8d6f82d3ff8f03ec
Benedict Etzel <benedict@hearthsim.net>
2018-03-14 04:32:06 +0100

    Add region name to pretty Blizzard accounts

--- hsreplaynet/static/scripts/src/utils/account.ts
+++ hsreplaynet/static/scripts/src/utils/account.ts
@@ -1,9 +1,19 @@
 import { BlizzardAccount } from "./api";
+import { BnetRegion } from "../hearthstone";
 
 export function getAccountKey(account: BlizzardAccount): string {
 	return `${account.region}-${account.account_lo}`;
 }
 
+const REGION_NAMES = {
+	[BnetRegion.REGION_US]: "North America",
+	[BnetRegion.REGION_EU]: "Europe",
+	[BnetRegion.REGION_KR]: "Korea",
+	[BnetRegion.REGION_CN]: "China",
+	[BnetRegion.REGION_TW]: "South East Asia",
+};
+
 export function prettyBlizzardAccount(account: BlizzardAccount): string {
-	return account.battletag;
+	const region = REGION_NAMES[account.region] || "Unknown region";
+	return `${account.battletag} (${region})`;
 }
