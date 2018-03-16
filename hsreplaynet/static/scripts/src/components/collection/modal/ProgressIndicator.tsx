214ee833df20a0cc0c724250f7b514a9e58d4d33
Benedict Etzel <benedict@hearthsim.net>
2018-03-16 18:06:24 +0100

    Add ProgressIndicator component

---
+++ hsreplaynet/static/scripts/src/components/collection/modal/ProgressIndicator.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+interface Props {
+	progress: number;
+	total: number;
+	"aria-labelledby"?: string;
+}
+
+export default class ProgressIndicator extends React.Component<Props> {
+	public render(): React.ReactNode {
+		if (this.props.total < 1) {
+			return null;
+		}
+
+		const steps = [];
+		for (let i = 0; i < this.props.total; i++) {
+			const step = i + 1;
+			const classNames = ["progress-indicator-step"];
+			if (step <= this.props.progress) {
+				classNames.push("active");
+			}
+			steps[i] = <div key={step} className={classNames.join(" ")} />;
+		}
+
+		const progress = steps.reduce<React.ReactNode[]>(
+			(prev, current, index) => {
+				const step = index + 1;
+				const classNames = ["progress-indicator-join"];
+				if (step <= this.props.progress) {
+					classNames.push("active");
+				}
+				if (prev.length > 0) {
+					return prev.concat(
+						<div
+							key={`join-${index}`}
+							className={classNames.join(" ")}
+						/>,
+						current,
+					);
+				}
+				return [current];
+			},
+			[],
+		);
+
+		return (
+			<div
+				className="progress-indicator"
+				role="progressbar"
+				aria-valuenow={this.props.progress}
+				aria-valuemin={1}
+				aria-valuemax={this.props.total}
+				aria-labelledby={this.props["aria-labelledby"]}
+			>
+				{progress}
+			</div>
+		);
+	}
+}
