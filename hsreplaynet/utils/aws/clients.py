be77352abbb919d14e6c9a5f512dd0ce52fc3900
Jerome Leclanche <jerome@leclan.ch>
2018-03-18 14:04:03 +0200

    Use localstack endpoints instead of moto

--- hsreplaynet/utils/aws/clients.py
+++ hsreplaynet/utils/aws/clients.py
@@ -3,33 +3,25 @@ from django.conf import settings
 
 
 def get_clients():
-	return (
-		boto3.client("iam"),
-		boto3.client("firehose", region_name=settings.AWS_DEFAULT_REGION),
-		boto3.client("kinesis", region_name=settings.AWS_DEFAULT_REGION),
-		boto3.client("lambda", region_name=settings.AWS_DEFAULT_REGION),
-		boto3.client("s3"),
-		boto3.client("sqs", region_name=settings.AWS_DEFAULT_REGION),
-	)
-
-
-if not settings.CONNECT_TO_AWS:
-	import moto
-
-	# https://github.com/spulec/moto
-	# If we're in an environment where we don't connect to upstream AWS
-	# (such as local dev), the following mocks the AWS services we use.
-
-	mocks = (
-		moto.mock_iam,
-		moto.mock_kinesis,
-		moto.mock_lambda,
-		moto.mock_s3,
-		moto.mock_sqs
-	)
-
-	for mock in mocks:
-		get_clients = mock(get_clients)
+	if settings.CONNECT_TO_AWS:
+		region = settings.AWS_DEFAULT_REGION
+		return (
+			boto3.client("iam"),
+			boto3.client("firehose", region_name=region),
+			boto3.client("kinesis", region_name=region),
+			boto3.client("lambda", region_name=region),
+			boto3.client("s3"),
+			boto3.client("sqs", region_name=region),
+		)
+	else:
+		return (
+			None,
+			boto3.client("firehose", endpoint_url="http://localhost:4573"),
+			boto3.client("kinesis", endpoint_url="http://localhost:4568"),
+			boto3.client("lambda", endpoint_url="http://localhost:4574"),
+			boto3.client("s3", endpoint_url="http://localhost:4572"),
+			boto3.client("sqs", endpoint_url="http://localhost:4576"),
+		)
 
 
 IAM, FIREHOSE, KINESIS, LAMBDA, S3, SQS = get_clients()
