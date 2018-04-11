b545c0f377da1094ccf986e64ce896983c85e2e3
Alexander Zeier <alex@hearthsim.net>
2018-04-12 00:03:24 +0200

    Fix web_test for new front page

--- tests/test_web.py
+++ tests/test_web.py
@@ -13,9 +13,5 @@ def test_load_homepage(client, settings, mocker):
 	# Disable (Manifest)StaticFilesStorage, since tests will not run collectstatic
 	settings.STATICFILES_STORAGE = settings.DEFAULT_FILE_STORAGE
 
-	# Until we start mocking aws.utils.redshift, mock HomeView.get_winrate_data directly
-	with mocker.patch("hsreplaynet.web.views.HomeView.get_winrate_data") as get_winrate_data:
-		get_winrate_data.return_value = {"standard": {}, "wild": {}}
-
 	response = client.get("/")
 	assert response.status_code == 200
