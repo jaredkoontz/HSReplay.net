05f5afb896b3912c9b1dc0f76df095cb8723f955
Jerome Leclanche <jerome@leclan.ch>
2018-04-15 13:52:21 +0300

    Remove legacy permissions from PackViewSet

--- hsreplaynet/packs/api.py
+++ hsreplaynet/packs/api.py
@@ -2,11 +2,10 @@ from rest_framework import serializers
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.exceptions import ValidationError
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
-from hsreplaynet.api.legacy import (
-	AuthTokenAuthentication, LegacyAPIKeyPermission, RequireAuthToken
-)
+from hsreplaynet.api.legacy import AuthTokenAuthentication
 
 from .models import Pack, PackCard
 
@@ -49,6 +48,6 @@ class PackSerializer(serializers.HyperlinkedModelSerializer):
 
 class PackViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
 	authentication_classes = (SessionAuthentication, AuthTokenAuthentication)
-	permission_classes = (RequireAuthToken, LegacyAPIKeyPermission)
+	permission_classes = (IsAuthenticated, )
 	queryset = Pack.objects.all()
 	serializer_class = PackSerializer
