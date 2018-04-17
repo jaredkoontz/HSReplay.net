4153f947a11e442252a4e2f5accba8960e5cee7b
Benedict Etzel <benedict@hearthsim.net>
2018-04-17 23:56:17 +0200

    Setup initial i18next context

--- hsreplaynet/static/scripts/src/components/Root.tsx
+++ hsreplaynet/static/scripts/src/components/Root.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ErrorReporter from "./ErrorReporter";
 import { Provider as HearthstoneAccountProvider } from "./utils/hearthstone-account";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n";
 
 interface Props {}
 
 export default class Root extends React.Component<Props> {
 	public render(): React.ReactNode {
 		return (
-			<ErrorReporter>
-				<HearthstoneAccountProvider>
-					{this.props.children}
-				</HearthstoneAccountProvider>
-			</ErrorReporter>
+			<I18nextProvider i18n={i18n}>
+				<ErrorReporter>
+					<HearthstoneAccountProvider>
+						{this.props.children}
+					</HearthstoneAccountProvider>
+				</ErrorReporter>
+			</I18nextProvider>
 		);
 	}
 }
---
+++ hsreplaynet/static/scripts/src/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+
+i18n
+	.init({
+		lng: "de",
+
+		// keys as strings
+		nsSeparator: false,
+		keySeparator: false,
+		fallbackLng: false,
+
+		// not required using i18next-react
+		interpolation: {
+			escapeValue: false,
+		},
+
+		// debug messages
+		debug: true,
+	})
+	.addResourceBundle("de", "translation", {
+		"Game Mode": "Spielmodus",
+		"Ranked Standard": "Standard (gewertet)",
+		"Ranked Wild": "Wild (gewertet)",
+		Arena: "Arena",
+	});
+
+export default i18n;
