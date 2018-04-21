a84b382ee6f3b81de673ceeccb11c0a51e0cdfec
Benedict Etzel <benedict@hearthsim.net>
2018-04-21 15:02:54 +0200

    DustFilter: Improve text input
    Specifically allow an initial backspace to get rid of the zero.

--- hsreplaynet/static/scripts/src/components/filters/DustFilter.tsx
+++ hsreplaynet/static/scripts/src/components/filters/DustFilter.tsx
@@ -9,10 +9,33 @@ interface Props {
 	id?: string;
 }
 
-export default class DustFilter extends React.Component<Props> {
+interface State {
+	input: string;
+}
+
+export default class DustFilter extends React.Component<Props, State> {
 	static defaultProps = {
 		maxDust: 50000,
 	};
+
+	constructor(props: Props, context: any) {
+		super(props, context);
+		this.state = {
+			input: this.props.dust !== null ? "" + this.props.dust : "0",
+		};
+	}
+
+	public static getDerivedStateFromProps(
+		nextProps: Readonly<Props>,
+		prevState: State,
+	): Partial<State> | null {
+		if (nextProps.dust !== null) {
+			return {
+				input: "" + nextProps.dust,
+			};
+		}
+		return null;
+	}
 
 	public render(): React.ReactNode {
 		const onClick = (value: number) => {
@@ -21,8 +44,6 @@ export default class DustFilter extends React.Component<Props> {
 		const isActive = (value: number) => {
 			return this.props.dust === value;
 		};
-		const ownedDust = Math.max(0, this.props.ownedDust);
-		const maxDust = Math.max(ownedDust, this.props.maxDust);
 
 		return (
 			<div className="dust-filter">
@@ -42,23 +63,53 @@ export default class DustFilter extends React.Component<Props> {
 					type="owned"
 					onClick={onClick}
 					isActive={isActive}
-					value={ownedDust}
+					value={this.ownedDust}
 				/>
 				<input
 					id={this.props.id || undefined}
 					type="number"
 					step={100}
-					value={this.props.dust}
+					value={this.state.input}
 					min={0}
-					max={maxDust}
-					onChange={e => {
-						this.props.setDust(
-							Math.max(0, Math.min(maxDust, +e.target.value)),
-						);
-					}}
+					max={this.maxDust}
+					onChange={this.onChange}
+					onKeyDown={this.onKeyDown}
+					onBlur={this.commit}
 					className="form-control"
 				/>
 			</div>
 		);
+	}
+
+	private onChange = (e): void => {
+		let input = e.target.value;
+		const num = +input;
+		if (!isNaN(num) && num > this.maxDust) {
+			input = "" + this.maxDust;
+		}
+		this.setState({ input }, () => {
+			if (this.state.input === "" + num) {
+				this.commit();
+			}
+		});
+	};
+
+	private onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+		if (e.key === "Enter") {
+			this.commit();
+		}
+	};
+
+	private commit = (): void => {
+		const num = Math.floor(+this.state.input);
+		this.props.setDust(Math.max(0, Math.min(this.maxDust, num)));
+	};
+
+	private get ownedDust(): number {
+		return Math.max(0, this.props.ownedDust);
+	}
+
+	private get maxDust(): number {
+		return Math.max(this.ownedDust, this.props.maxDust);
 	}
 }
