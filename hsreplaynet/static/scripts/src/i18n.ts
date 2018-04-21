987e6aa152515f90f1a06060433891414dd989fa
Benedict Etzel <benedict@hearthsim.net>
2018-04-21 21:08:08 +0200

    Prettier fixes

--- hsreplaynet/static/scripts/src/i18n.ts
+++ hsreplaynet/static/scripts/src/i18n.ts
@@ -1,16 +1,15 @@
 import i18n from "i18next";
 
-i18n
-	.init({
-		// keys as strings
-		nsSeparator: false,
-		keySeparator: false,
-		fallbackLng: false,
+i18n.init({
+	// keys as strings
+	nsSeparator: false,
+	keySeparator: false,
+	fallbackLng: false,
 
-		// not required using i18next-react
-		interpolation: {
-			escapeValue: false,
-		},
-	});
+	// not required using i18next-react
+	interpolation: {
+		escapeValue: false,
+	},
+});
 
 export default i18n;
