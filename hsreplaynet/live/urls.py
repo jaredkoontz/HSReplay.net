1215b8310463d94f005f985e8d7c125b1abc26af
Jerome Leclanche <jerome@leclan.ch>
2018-04-27 10:46:49 +0300

    Rework live views into DRF-style APIViews

--- hsreplaynet/live/urls.py
+++ hsreplaynet/live/urls.py
@@ -6,28 +6,17 @@ from . import views
 urlpatterns = [
 	path(
 		"distributions/player_class/<game_type_name>/",
-		views.fetch_player_class_distribution,
-		name="live_fetch_player_class_distribution"
+		views.PlayerClassDistributionView.as_view()
 	),
 	path(
 		"distributions/played_cards/",
-		views.fetch_played_cards_distribution,
-		name="live_fetch_played_cards_distribution"
+		views.PlayedCardsDistributionView.as_view()
 	),
 	path(
 		"distributions/played_cards/<game_type_name>/",
-		views.fetch_played_cards_distribution_for_gametype,
-		name="live_fetch_played_cards_distribution_for_gametype"
+		views.GameTypePlayedCardsDistributionView.as_view()
 	),
-	path(
-		"games_count/weekly/",
-		views.fetch_weekly_games_count,
-		name="live_weekly_games_count"
-	),
-	path(
-		"replay_feed/", views.fetch_replay_feed, name="live_replay_feed"
-	),
-	path(
-		"streaming-now/", views.StreamingNowView.as_view(),
-	)
+	path("games_count/weekly/", views.WeeklyGamesCountView.as_view()),
+	path("replay_feed/", views.LiveReplayFeedView.as_view()),
+	path("streaming-now/", views.StreamingNowView.as_view()),
 ]
--- hsreplaynet/live/views.py
+++ hsreplaynet/live/views.py
@@ -3,12 +3,12 @@ from datetime import datetime, timedelta
 
 from allauth.socialaccount.models import SocialAccount
 from django.core.cache import caches
-from django.http import Http404, JsonResponse
+from django.http import Http404
 from hearthstone.enums import BnetGameType
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from hsreplaynet.live.distributions import (
+from .distributions import (
 	get_daily_contributor_set, get_daily_game_counter, get_live_stats_redis,
 	get_played_cards_distribution, get_player_class_distribution, get_replay_feed
 )
@@ -17,6 +17,7 @@ from hsreplaynet.live.distributions import (
 _PLAYER_CLASS_CACHE = defaultdict(dict)
 _REPLAY_FEED_CACHE = defaultdict()
 _WEEKLY_GAMES_COUNT = defaultdict()
+_PLAYED_CARDS_CACHE = defaultdict(dict)
 
 
 def _get_base_ts(bucket_size=5):
@@ -40,105 +41,130 @@ def _get_most_recent_tick_ts(tick=5):
 	return most_recent_tick_ts
 
 
-def _validate_game_type(game_type_name):
+def _validate_game_type(game_type_name: str):
 	if not hasattr(BnetGameType, game_type_name):
 		raise Http404("Invalid GameType")
 
 
-def fetch_replay_feed(request):
-	"""Return the last n uploaded replays"""
-	current_ts = datetime.utcnow().timestamp()
+class LiveReplayFeedView(APIView):
+	def get(self, request):
+		current_ts = datetime.utcnow().timestamp()
 
-	if _REPLAY_FEED_CACHE.get("as_of", 0) + 5 < current_ts:
-		feed = get_replay_feed()
-		_REPLAY_FEED_CACHE["as_of"] = current_ts
-		_REPLAY_FEED_CACHE["payload"] = feed.get(200)
+		if _REPLAY_FEED_CACHE.get("as_of", 0) + 5 < current_ts:
+			feed = get_replay_feed()
+			_REPLAY_FEED_CACHE["as_of"] = current_ts
+			_REPLAY_FEED_CACHE["payload"] = feed.get(200)
 
-	return JsonResponse(
-		{"data": _REPLAY_FEED_CACHE.get("payload", [])},
-		json_dumps_params=dict(indent=4)
-	)
+		data = {"data": _REPLAY_FEED_CACHE.get("payload", [])}
+		return Response(data=data)
 
 
-def fetch_weekly_games_count(request):
-	"""Return the games counts for the last 7 days"""
-	current_ts = datetime.utcnow().timestamp()
+class WeeklyGamesCountView(APIView):
+	def get(self, request):
+		current_ts = datetime.utcnow().timestamp()
 
-	if _WEEKLY_GAMES_COUNT.get("as_of", 0) + 5 < current_ts:
-		counter = get_daily_game_counter()
-		contributors = get_daily_contributor_set()
-		_WEEKLY_GAMES_COUNT["as_of"] = current_ts
-		_WEEKLY_GAMES_COUNT["payload"] = {
-			"games_today": counter.get_count(0, 0),
-			"games_weekly": counter.get_count(1, 7),
-			"contributors_today": contributors.get_count(0, 0),
-			"contributors_weekly": contributors.get_count(1, 7)
-		}
+		if _WEEKLY_GAMES_COUNT.get("as_of", 0) + 5 < current_ts:
+			counter = get_daily_game_counter()
+			contributors = get_daily_contributor_set()
+			_WEEKLY_GAMES_COUNT["as_of"] = current_ts
+			_WEEKLY_GAMES_COUNT["payload"] = {
+				"games_today": counter.get_count(0, 0),
+				"games_weekly": counter.get_count(1, 7),
+				"contributors_today": contributors.get_count(0, 0),
+				"contributors_weekly": contributors.get_count(1, 7)
+			}
 
-	return JsonResponse(
-		{"data": _WEEKLY_GAMES_COUNT.get("payload", {})},
-		json_dumps_params=dict(indent=4)
-	)
+		data = {"data": _WEEKLY_GAMES_COUNT.get("payload", {})}
 
-
-def fetch_player_class_distribution(request, game_type_name):
-	"""Return the last 60 seconds of player class data using a 5 minute sliding window"""
-	_validate_game_type(game_type_name)
-
-	# How many seconds back in time to start.
-	lookback = int(request.GET.get("lookback", 600))
-	# How many seconds are in the window we calculate the distribution over.
-	window = int(request.GET.get("window", 300))
-	# How many seconds between data points.
-	tick = int(request.GET.get("tick", 5))
-
-	player_class_popularity = get_player_class_distribution(game_type_name)
-
-	# base_ts ensures we generate the result at most once per bucket_size seconds
-	most_recent_tick_ts = _get_most_recent_tick_ts(tick=tick)
-	start_ts = most_recent_tick_ts - timedelta(seconds=lookback)
-	end_ts = start_ts + timedelta(seconds=window)
-
-	if _PLAYER_CLASS_CACHE[game_type_name].get("as_of", None) != most_recent_tick_ts:
-		result = []
-		while end_ts <= most_recent_tick_ts:
-			data = player_class_popularity.distribution(
-				start_ts=start_ts,
-				end_ts=end_ts
-			)
-			result.append({
-				"ts": int(end_ts.timestamp()),
-				"data": data
-			})
-			start_ts = start_ts + timedelta(seconds=tick)
-			end_ts = start_ts + timedelta(seconds=window)
-
-		_PLAYER_CLASS_CACHE[game_type_name]["as_of"] = most_recent_tick_ts
-		_PLAYER_CLASS_CACHE[game_type_name]["payload"] = result
-
-	return JsonResponse(
-		{"data": _PLAYER_CLASS_CACHE[game_type_name].get("payload", [])},
-		json_dumps_params=dict(indent=4)
-	)
+		return Response(data=data)
 
 
-_PLAYED_CARDS_CACHE = defaultdict(dict)
+class PlayerClassDistributionView(APIView):
+	def get(self, request, game_type_name: str) -> Response:
+		_validate_game_type(game_type_name)
+
+		# How many seconds back in time to start.
+		lookback = int(request.GET.get("lookback", 600))
+		# How many seconds are in the window we calculate the distribution over.
+		window = int(request.GET.get("window", 300))
+		# How many seconds between data points.
+		tick = int(request.GET.get("tick", 5))
+
+		player_class_popularity = get_player_class_distribution(game_type_name)
+
+		# base_ts ensures we generate the result at most once per bucket_size seconds
+		most_recent_tick_ts = _get_most_recent_tick_ts(tick=tick)
+		start_ts = most_recent_tick_ts - timedelta(seconds=lookback)
+		end_ts = start_ts + timedelta(seconds=window)
+
+		if _PLAYER_CLASS_CACHE[game_type_name].get("as_of", None) != most_recent_tick_ts:
+			result = []
+			while end_ts <= most_recent_tick_ts:
+				data = player_class_popularity.distribution(
+					start_ts=start_ts,
+					end_ts=end_ts
+				)
+				result.append({
+					"ts": int(end_ts.timestamp()),
+					"data": data
+				})
+				start_ts = start_ts + timedelta(seconds=tick)
+				end_ts = start_ts + timedelta(seconds=window)
+
+			_PLAYER_CLASS_CACHE[game_type_name]["as_of"] = most_recent_tick_ts
+			_PLAYER_CLASS_CACHE[game_type_name]["payload"] = result
+
+		data = {"data": _PLAYER_CLASS_CACHE[game_type_name].get("payload", [])}
+		return Response(data=data)
 
 
-def fetch_played_cards_distribution(request):
-	# base_ts ensures we generate the result at most once per bucket_size seconds
-	base_ts = _get_base_ts(bucket_size=5)
-
+class PlayedCardsDistributionView(APIView):
 	eligible_game_types = [
 		BnetGameType.BGT_RANKED_WILD,
 		BnetGameType.BGT_RANKED_STANDARD,
 		BnetGameType.BGT_ARENA
 	]
 
-	if _PLAYER_CLASS_CACHE["ALL"].get("as_of", None) != base_ts:
-		payload = {}
-		for game_type in eligible_game_types:
-			played_cards_popularity = get_played_cards_distribution(game_type.name)
+	def get(self, request):
+		# base_ts ensures we generate the result at most once per bucket_size seconds
+		base_ts = _get_base_ts(bucket_size=5)
+
+		if _PLAYER_CLASS_CACHE["ALL"].get("as_of", None) != base_ts:
+			payload = {}
+			for game_type in self.eligible_game_types:
+				played_cards_popularity = get_played_cards_distribution(game_type.name)
+				result = []
+				for i in range(0, 61, 5):
+					end_ts = base_ts + timedelta(seconds=i)
+					start_ts = end_ts - timedelta(seconds=300)
+					data = played_cards_popularity.distribution(
+						start_ts=start_ts,
+						end_ts=end_ts,
+						limit=20
+					)
+					result.append({
+						"ts": int(end_ts.timestamp()),
+						"data": data
+					})
+				payload[game_type.name] = result
+
+			_PLAYER_CLASS_CACHE["ALL"]["as_of"] = base_ts
+			_PLAYER_CLASS_CACHE["ALL"]["payload"] = payload
+
+		data = _PLAYER_CLASS_CACHE["ALL"].get("payload", {})
+		return Response(data=data)
+
+
+class GameTypePlayedCardsDistributionView(APIView):
+	def get(self, request, game_type_name: str) -> Response:
+		_validate_game_type(game_type_name)
+
+		played_cards_popularity = get_played_cards_distribution(game_type_name)
+
+		# base_ts ensures we generate the result at most once per bucket_size seconds
+		base_ts = _get_base_ts(bucket_size=5)
+
+		if _PLAYED_CARDS_CACHE[game_type_name].get("as_of") != base_ts:
 			result = []
 			for i in range(0, 61, 5):
 				end_ts = base_ts + timedelta(seconds=i)
@@ -146,54 +172,17 @@ def fetch_played_cards_distribution(request):
 				data = played_cards_popularity.distribution(
 					start_ts=start_ts,
 					end_ts=end_ts,
-					limit=20
+					limit=10
 				)
 				result.append({
 					"ts": int(end_ts.timestamp()),
 					"data": data
 				})
-			payload[game_type.name] = result
+			_PLAYED_CARDS_CACHE[game_type_name]["as_of"] = base_ts
+			_PLAYED_CARDS_CACHE[game_type_name]["payload"] = result
 
-		_PLAYER_CLASS_CACHE["ALL"]["as_of"] = base_ts
-		_PLAYER_CLASS_CACHE["ALL"]["payload"] = payload
-
-	return JsonResponse(
-		_PLAYER_CLASS_CACHE["ALL"].get("payload", {}),
-		json_dumps_params=dict(indent=4)
-	)
-
-
-def fetch_played_cards_distribution_for_gametype(request, game_type_name):
-	"""Return the last 60 seconds of played cards data using a 5 minute sliding window"""
-
-	_validate_game_type(game_type_name)
-
-	played_cards_popularity = get_played_cards_distribution(game_type_name)
-
-	# base_ts ensures we generate the result at most once per bucket_size seconds
-	base_ts = _get_base_ts(bucket_size=5)
-
-	if _PLAYED_CARDS_CACHE[game_type_name].get("as_of", None) != base_ts:
-		result = []
-		for i in range(0, 61, 5):
-			end_ts = base_ts + timedelta(seconds=i)
-			start_ts = end_ts - timedelta(seconds=300)
-			data = played_cards_popularity.distribution(
-				start_ts=start_ts,
-				end_ts=end_ts,
-				limit=10
-			)
-			result.append({
-				"ts": int(end_ts.timestamp()),
-				"data": data
-			})
-		_PLAYED_CARDS_CACHE[game_type_name]["as_of"] = base_ts
-		_PLAYED_CARDS_CACHE[game_type_name]["payload"] = result
-
-	return JsonResponse(
-		{"data": _PLAYED_CARDS_CACHE[game_type_name].get("payload", [])},
-		json_dumps_params=dict(indent=4)
-	)
+		data = {"data": _PLAYED_CARDS_CACHE[game_type_name].get("payload", [])}
+		return Response(data=data)
 
 
 class StreamingNowView(APIView):
