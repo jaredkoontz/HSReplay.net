f70f0c21bbc44e05fcc291ee6662fcfcdfef3d50
Jerome Leclanche <jerome@leclan.ch>
2018-04-27 10:28:00 +0300

    Convert url to path in live.urls

--- hsreplaynet/live/urls.py
+++ hsreplaynet/live/urls.py
@@ -1,36 +1,33 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
-	url(
-		r"^distributions/player_class/(?P<game_type_name>\w+)/$",
+	path(
+		"distributions/player_class/<game_type_name>/",
 		views.fetch_player_class_distribution,
 		name="live_fetch_player_class_distribution"
 	),
-	url(
-		r"^distributions/played_cards/$",
+	path(
+		"distributions/played_cards/",
 		views.fetch_played_cards_distribution,
 		name="live_fetch_played_cards_distribution"
 	),
-	url(
-		r"^distributions/played_cards/(?P<game_type_name>\w+)/$",
+	path(
+		"distributions/played_cards/<game_type_name>/",
 		views.fetch_played_cards_distribution_for_gametype,
 		name="live_fetch_played_cards_distribution_for_gametype"
 	),
-	url(
-		r"^games_count/weekly/$",
+	path(
+		"games_count/weekly/",
 		views.fetch_weekly_games_count,
 		name="live_weekly_games_count"
 	),
-	url(
-		r"^replay_feed/$",
-		views.fetch_replay_feed,
-		name="live_replay_feed"
+	path(
+		"replay_feed/", views.fetch_replay_feed, name="live_replay_feed"
 	),
-	url(
-		r"^streaming-now/$",
-		views.StreamingNowView.as_view(),
+	path(
+		"streaming-now/", views.StreamingNowView.as_view(),
 	)
 ]
