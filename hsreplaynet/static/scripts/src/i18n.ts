3cdad2278af6bce024e95a912016874a296a6e1a
Jerome Leclanche <jerome@leclan.ch>
2018-04-29 21:23:15 +0300

    Add i18next-scanner configuration
    Usable by running "yarn i18n:extract".

--- hsreplaynet/static/scripts/src/i18n.ts
+++ hsreplaynet/static/scripts/src/i18n.ts
@@ -1,10 +1,13 @@
 import i18n from "i18next";
 
+const I18N_NAMESPACE = "frontend";
+
 i18n.init({
 	// keys as strings
-	nsSeparator: false,
-	keySeparator: false,
+	defaultNS: I18N_NAMESPACE,
 	fallbackLng: false,
+	keySeparator: false,
+	nsSeparator: false,
 
 	// not required using i18next-react
 	interpolation: {
