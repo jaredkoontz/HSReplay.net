a56220a03145ca5deba86af029b76cbf0a903ef9
Benedict Etzel <benedict@hearthsim.net>
2018-05-03 15:29:36 +0200

    Remove filename from i18next-scanner output

--- i18next-scanner.config.js
+++ i18next-scanner.config.js
@@ -1,38 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-
-const KEY_SEPARATOR = "__KEY_SEPARATOR__";
-
 module.exports = {
 	options: {
 		// use strings as keys
 		defaultNs: "frontend",
 		nsSeparator: false,
-		keySeparator: KEY_SEPARATOR,
-		func: false,
+		keySeparator: false,
 		lngs: ["en"],
 		resource: {
 			loadPath: "{{lng}}/{{ns}}.json",
 			savePath: "{{lng}}/{{ns}}.json",
 		},
-	},
-	transform: function customTransform(file, enc, done) {
-		"use strict";
-		const parser = this.parser;
-		const content = fs.readFileSync(file.path, enc);
-
-		parser.parseFuncFromString(content, { list: ["t"] }, (key, options) => {
-			const base = "hsreplaynet/static/scripts/src";
-			const fileContext = path.relative(
-				path.join(__dirname, base),
-				file.path,
-			);
-
-			parser.set([fileContext, key].join(KEY_SEPARATOR), {
-				defaultValue: key,
-			});
-		});
-
-		done();
+		func: {
+			list: ["t"],
+			extensions: [".js", ".jsx", ".ts", ".tsx"],
+		},
+		defaultValue: (language, namespace, key) => key,
 	},
 };
