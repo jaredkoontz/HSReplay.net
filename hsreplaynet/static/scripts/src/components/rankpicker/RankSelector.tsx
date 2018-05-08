ebf8cd4f910ac85ee0299f1d37ef20e941d78525
Benedict Etzel <benedict@hearthsim.net>
2018-05-08 14:51:23 +0200

    Translate RankSelector

--- hsreplaynet/static/scripts/src/components/rankpicker/RankSelector.tsx
+++ hsreplaynet/static/scripts/src/components/rankpicker/RankSelector.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { image } from "../../helpers";
+import { InjectedTranslateProps, translate } from "react-i18next";
 
-interface Props {
+interface Props extends InjectedTranslateProps {
 	classNames: string[];
 	onClick: () => void;
 	rank: number;
 }
 
-export default class RankSelector extends React.Component<Props> {
+class RankSelector extends React.Component<Props> {
 	public render(): React.ReactNode {
-		const { rank } = this.props;
+		const { t, rank } = this.props;
 		const classNames = ["rank-selector"].concat(this.props.classNames);
-		const rankStr = rank === 0 ? "Legend" : rank;
+		const rankStr = rank === 0 ? t("Legend") : rank;
+		const altStr = rank === 0 ? t("Legend") : t("Rank {{rank}}", { rank });
 		return (
 			<div className={classNames.join(" ")} onClick={this.props.onClick}>
 				<img
 					src={image(`64x/ranked-medals/Medal_Ranked_${rankStr}.png`)}
+					alt={altStr}
 				/>
 				<span>{rankStr}</span>
 			</div>
 		);
 	}
 }
+
+export default translate()(RankSelector);
