818b8e9369bfaacfb734aec423341037d03cd356
Jerome Leclanche <jerome@leclan.ch>
2018-05-13 09:16:01 +0300

    Rework and redesign account delete page

---
+++ hsreplaynet/static/scripts/src/components/account/AccountDeleteReason.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+interface Props {
+	value: string;
+	reason: string;
+}
+
+interface State {
+	checked: boolean;
+}
+
+export default class AccountDeleteReason extends React.Component<Props, State> {
+	constructor(props: Props, context?: any) {
+		super(props, context);
+		this.state = {
+			checked: false,
+		};
+	}
+
+	public render(): React.ReactNode {
+		return (
+			<li className="radio">
+				<label className="radio">
+					<input
+						type="radio"
+						name="reason"
+						value={this.props.value}
+						onChange={(e: React.FormEvent<HTMLInputElement>) => {
+							this.setState({
+								checked:
+									e.currentTarget.value === this.props.value,
+							});
+						}}
+						required
+					/>
+					{this.props.reason}
+				</label>
+				{this.props.children && this.state.checked ? (
+					<p className="alert alert-warning">{this.props.children}</p>
+				) : (
+					<></>
+				)}
+			</li>
+		);
+	}
+}
--- hsreplaynet/static/scripts/src/pages/AccountDelete.tsx
+++ hsreplaynet/static/scripts/src/pages/AccountDelete.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { InjectedTranslateProps, Trans, translate } from "react-i18next";
 import UserData from "../UserData";
 import CSRFElement from "../components/CSRFElement";
+import AccountDeleteReason from "../components/account/AccountDeleteReason";
 
 interface Props extends InjectedTranslateProps {}
 interface State {}
@@ -30,78 +31,149 @@ class AccountDelete extends React.Component<Props, State> {
 			<section className="box-section">
 				<h3>{t("Delete account")}</h3>
 				<div className="inner">
-					<p className="alert alert-warning">
+					<p>
 						<Trans>
-							<strong>
-								Did you log in with the wrong Blizzard account?
-							</strong>
-							<br />
-							You can link multiple Blizzard accounts{" "}
-							<a href="/account/social/connections/#account-connections">
-								in the Connected accounts section
-							</a>. If you need to merge an account,{" "}
+							We're sorry to see you go. You can reach out to us
+							by email at{" "}
 							<a href="mailto:contact@hsreplay.net">
-								contact us directly
+								contact@hsreplay.net
+							</a>,{" "}
+							<a href="https://discord.gg/hearthsim">
+								on Discord
+							</a>, or on Twitter{" "}
+							<a href="https://twitter.com/HSReplayNet">
+								@HSReplayNet
 							</a>.
 						</Trans>
 					</p>
+					<Trans>
+						<p>Deleting your account will:</p>
+						<ul>
+							<li>
+								Remove all your personal information from the
+								site
+							</li>
+							<li>
+								Unlink your Blizzard, Discord and Twitch
+								accounts
+							</li>
+							<li>
+								Unlink any application linked to your
+								HSReplay.net account (such as Hearthstone Deck
+								Tracker).
+							</li>
+						</ul>
+					</Trans>
+					<Trans>
+						<p>
+							Deleting your account <strong>will not</strong>:
+						</p>
+						<ul>
+							<li>
+								Reset your statistics. Games are tracked across
+								the entire playerbase.
+							</li>
+							<li>
+								Delete your replays. If you wish to delete your
+								replays, you can do so <a href="#">here</a>.
+							</li>
+							<li>
+								Delete any billing data, or any records we are
+								legally obligated to keep.
+							</li>
+						</ul>
+					</Trans>
 
-					<p>
-						<Trans>
-							Problems with the site? Something you're not happy
-							about? Maybe we can fix it.{" "}
-							<a href="mailto:{{ site_email }}">
-								Shoot us an email
-							</a>!
-						</Trans>
-					</p>
-					<form action={""} method="POST">
+					<form
+						action={""}
+						method="POST"
+						onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+							if (
+								!confirm(
+									t(
+										"Deleting your account is immediate and cannot be undone. Are you sure you want to proceed?",
+									),
+								)
+							) {
+								e.preventDefault();
+							}
+						}}
+					>
 						<CSRFElement />
-						<p>
-							{t(
-								"Once your data is gone, it's irreversible. Are you sure?",
-							)}
-						</p>
-						<p className={"form-group"}>
-							<label>
-								<input
-									name="delete_confirm"
-									type="checkbox"
-									required
-								/>{" "}
-								<strong>
-									{t("I understand. Delete my account.")}
-								</strong>
-							</label>
-							<br />
-							<label>
-								<input name="delete_replays" type="checkbox" />{" "}
-								{t("Also delete my replay data")}
-							</label>
-						</p>
 
-						<p>
-							<textarea
-								name="delete_reason"
-								placeholder={t("Tell us why you're leaving")}
-								cols={50}
-								rows={5}
-								className="form-control"
-							/>
-						</p>
-						<p>
-							<em>
-								{t(
-									"Note: Accounts are deleted after a week. Logging back in before that will cancel the request.",
+						<h4>{t("Reason for leaving")}</h4>
+						<ul>
+							<AccountDeleteReason
+								value="multiple-accounts"
+								reason={t("I have another Blizzard account")}
+							>
+								Please{" "}
+								<a href="mailto:contact@hsreplay.net">
+									contact us
+								</a>, we can help you by merging your accounts!
+							</AccountDeleteReason>
+							<AccountDeleteReason
+								value="dont-want-premium"
+								reason={t("I don't want to pay for Premium")}
+							>
+								HSReplay.net is entirely usable for free!
+								Although Premium is how the site stays afloat,
+								you do not have to pay to keep using it.
+							</AccountDeleteReason>
+							<AccountDeleteReason
+								value="reset-stats"
+								reason={t(
+									"I want to reset my replays or statistics",
 								)}
-							</em>
-						</p>
-						<p className={"form-group"}>
+							>
+								<strong>Wait!</strong> Deleting your account
+								will not currently reset your statistics. This
+								is because we track everyone's statistics
+								globally.<br />If you want to delete your
+								replays, you can simply do so by{" "}
+								<a href="#">clicking here</a>.{" "}
+							</AccountDeleteReason>
+							<AccountDeleteReason
+								value="stopped-hearthstone"
+								reason={t("I stopped playing Hearthstone")}
+							/>
+							<AccountDeleteReason
+								value="site-problem"
+								reason={t("I have a problem with the site")}
+							>
+								If you're encountering issues, we want to know
+								about them. You can contact us at{" "}
+								<a href="mailto:contact@hsreplay.net">
+									contact@hsreplay.net
+								</a>{" "}
+								or{" "}
+								<a href="https://discord.gg/hearthsim">
+									reach out on Discord
+								</a>. We answer every email.
+							</AccountDeleteReason>
+							<AccountDeleteReason
+								value="other"
+								reason={t("Other (please explain further):")}
+							/>
+						</ul>
+
+						<h4>{t("Please explain further")}</h4>
+						<textarea
+							name="message"
+							cols={50}
+							rows={5}
+							className="form-control"
+						/>
+
+						<p className="form-group">
 							<input
 								type="submit"
 								className="btn btn-danger"
-								value={t("Confirm")}
+								value={t("Delete account")}
 							/>
+							<a href="/" className="btn btn-info">
+								{t("Cancel")}
+							</a>
 						</p>
 					</form>
 				</div>
