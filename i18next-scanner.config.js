7f0b89d7651c0e15cb4c15052eafcf82bcf775e9
Benedict Etzel <benedict@hearthsim.net>
2018-05-17 17:31:32 +0200

    Blacklist Hearthstone keys as translation keys
    Closes #789.

--- i18next-scanner.config.js
+++ i18next-scanner.config.js
@@ -1,3 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+let hearthstoneKeys = [];
+const hearthstoneDir = path.resolve(__dirname, "locale", "en", "hearthstone");
+const filenames = fs.readdirSync(hearthstoneDir);
+for (const filename of filenames) {
+	const file = fs.readFileSync(path.resolve(hearthstoneDir, filename));
+	const contents = JSON.parse(file);
+	hearthstoneKeys = hearthstoneKeys.concat(Object.keys(contents));
+}
+
 module.exports = {
 	options: {
 		// use strings as keys
@@ -10,16 +22,39 @@ module.exports = {
 			loadPath: "{{lng}}/{{ns}}.json",
 			savePath: "{{lng}}/{{ns}}.json",
 		},
-		func: {
-			list: ["t"],
-			extensions: [".js", ".jsx", ".ts", ".tsx"],
-		},
-		trans: {
-			component: "Trans",
-			i18nKey: false,
-			extensions: [".jsx", ".tsx"],
-			fallbackKey: (ns, value) => value,
-		},
+		func: false,
+		trans: false,
 		defaultValue: (language, namespace, key) => key,
+	},
+	transform(file, enc, done) {
+		const parser = this.parser;
+		const content = fs.readFileSync(file.path, enc);
+		const ext = path.extname(file.path);
+
+		parser.parseFuncFromString(
+			content,
+			{
+				list: ["t"],
+			},
+			(key, options) => {
+				if (hearthstoneKeys.indexOf(key) !== -1) {
+					return;
+				}
+				parser.set(key, options);
+			},
+		);
+
+		parser.parseTransFromString(
+			content,
+			{
+				component: ["Trans"],
+				i18nKey: false,
+			},
+			(key, options) => {
+				parser.set(options.defaultValue, options);
+			},
+		);
+
+		done();
 	},
 };
