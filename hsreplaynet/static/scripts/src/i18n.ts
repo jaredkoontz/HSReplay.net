8e33ffd1339de7d76c3aecf9cc4d0284fc04a96d
Benedict Etzel <benedict@hearthsim.net>
2018-05-24 22:23:21 +0200

    Revert "Experiment with reenabling i18next-icu memoization"
    This reverts commit e058b1901449e7c4abbc8914fe855086e111ff98.

--- hsreplaynet/static/scripts/src/i18n.ts
+++ hsreplaynet/static/scripts/src/i18n.ts
@@ -39,7 +39,7 @@ i18n
 			file names. There's not a lot data behind these though, so we just
 			hardcode the languages we support for now. */
 			localeData: [de, en, es, fr, it, ja, ko, pl, pt, ru, th, zh],
-			memoize: true, // temporarily enabled to check performance impact
+			memoize: false,
 		},
 
 		// not required using i18next-react
