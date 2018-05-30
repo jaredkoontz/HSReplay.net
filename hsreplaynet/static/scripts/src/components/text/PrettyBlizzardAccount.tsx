d484bb02ba503689e74cc9bc6f2f0d2c2a1b8065
Benedict Etzel <benedict@hearthsim.net>
2018-05-30 14:06:35 +0200

    Clean up PrettyBlizzardAccount.tsx

--- hsreplaynet/static/scripts/src/components/text/PrettyBlizzardAccount.tsx
+++ hsreplaynet/static/scripts/src/components/text/PrettyBlizzardAccount.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { InjectedTranslateProps, translate } from "react-i18next";
+import { Trans } from "react-i18next";
 import { BlizzardAccount as ApiBlizzardAccount } from "../../utils/api";
 import PrettyRegion from "./PrettyRegion";
 
-interface Props extends InjectedTranslateProps {
+interface Props {
 	account: ApiBlizzardAccount;
 }
 
 class PrettyBlizzardAccount extends React.Component<Props> {
 	public render(): React.ReactNode {
-		const { account, t } = this.props;
+		const { account } = this.props;
+		const battletag = account.battletag;
 		return (
-			<>
-				{account.battletag} (<PrettyRegion region={account.region} />)
-			</>
+			<Trans>
+				{battletag} (<PrettyRegion region={account.region} />)
+			</Trans>
 		);
 	}
 }
 
-export default translate()(PrettyBlizzardAccount);
+export default PrettyBlizzardAccount;
