3b75e03f54965f08948c9b03b79fe9ad01382a2c
Alexander Zeier <alex@hearthsim.net>
2018-07-21 12:29:38 -0700

    Revert "Revert "Reenable Hearthstone global strings""
    This reverts commit 3eeb105103f918b4e5b5df2c156d8e179bd645c5.

--- hsreplaynet/static/scripts/src/pages/CardDetail.tsx
+++ hsreplaynet/static/scripts/src/pages/CardDetail.tsx
@@ -34,13 +34,12 @@ import {
 	getChartScheme,
 	getDustCost,
 	getHeroClassName,
-	getSetName,
 	isArenaOnlyCard,
 	isCollectibleCard,
 	isWildSet,
 	toPrettyNumber,
-	toTitleCase,
 } from "../helpers";
+import { I18N_NAMESPACE_HEARTHSTONE } from "../i18n";
 import { RenderData, TableData } from "../interfaces";
 import { Collection } from "../utils/api";
 import LoadingSpinner from "../components/LoadingSpinner";
@@ -608,7 +607,9 @@ class CardDetail extends React.Component<Props, State> {
 				<li>
 					{t("Tribe")}
 					<span className="infobox-value">
-						{toTitleCase(this.props.card.race)}
+						{t(`GLOBAL_RACE_${this.props.card.race}`, {
+							ns: I18N_NAMESPACE_HEARTHSTONE,
+						})}
 					</span>
 				</li>
 			);
@@ -792,7 +793,12 @@ class CardDetail extends React.Component<Props, State> {
 							{t("Type")}
 							<span className="infobox-value">
 								{this.props.card &&
-									toTitleCase(this.props.card.type)}
+									t(
+										`GLOBAL_CARDTYPE_${
+											this.props.card.type
+										}`,
+										{ ns: I18N_NAMESPACE_HEARTHSTONE },
+									)}
 							</span>
 						</li>
 						{this.props.card && this.props.card.rarity ? (
@@ -800,7 +806,12 @@ class CardDetail extends React.Component<Props, State> {
 								{t("Rarity")}
 								<span className="infobox-value">
 									{this.props.card &&
-										toTitleCase(this.props.card.rarity)}
+										t(
+											`GLOBAL_RARITY_${
+												this.props.card.rarity
+											}`,
+											{ ns: I18N_NAMESPACE_HEARTHSTONE },
+										)}
 								</span>
 							</li>
 						) : null}
@@ -808,7 +819,12 @@ class CardDetail extends React.Component<Props, State> {
 							{t("Set")}
 							<span className="infobox-value">
 								{this.props.card &&
-									getSetName(this.props.card.set, t)}
+									t(
+										`GLOBAL_CARD_SET_${
+											this.props.card.set
+										}`,
+										{ ns: I18N_NAMESPACE_HEARTHSTONE },
+									)}
 							</span>
 						</li>
 						{tribe}
