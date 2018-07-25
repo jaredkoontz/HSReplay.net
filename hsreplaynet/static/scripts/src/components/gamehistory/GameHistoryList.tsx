fc39d73946ee075a8c62a67092aa733444f39191
Alexander Zeier <alex@hearthsim.net>
2018-07-24 23:46:06 -0700

    Add ads on my replays tiles

--- hsreplaynet/static/scripts/src/components/gamehistory/GameHistoryList.tsx
+++ hsreplaynet/static/scripts/src/components/gamehistory/GameHistoryList.tsx
@@ -7,6 +7,9 @@ import {
 	ImageProps,
 } from "../../interfaces";
 import GameHistoryItem from "./GameHistoryItem";
+import AdContainer from "../AdContainer";
+import AdUnit from "../AdUnit";
+import * as _ from "lodash";
 
 interface Props extends ImageProps, CardArtProps {
 	games: GameReplay[];
@@ -15,18 +18,48 @@ interface Props extends ImageProps, CardArtProps {
 export default class GameHistoryList extends React.Component<Props> {
 	public render(): React.ReactNode {
 		const columns = [];
+		const ads = _.range(4, 100, 2).map(x => (
+			<AdContainer>
+				<AdUnit id={`mr-d-${x}`} size="728x90" />
+				<AdUnit id={`mr-d-${x + 1}`} size="728x90" />
+			</AdContainer>
+		));
+		const getAd = id => {
+			return id < ads.length && id >= 0 ? ads[id] : null;
+		};
 		this.props.games.forEach((game: GameReplay, i: number) => {
 			const startTime: Date = new Date(game.global_game.match_start);
 			const endTime: Date = new Date(game.global_game.match_end);
 			if (i > 0) {
 				if (!(i % 2)) {
 					columns.push(<div className="clearfix visible-sm-block" />);
+					if (!(i % 4)) {
+						columns.push(
+							<div className="visible-sm">
+								{getAd(i / 4 - 1)}
+							</div>,
+						);
+					}
 				}
 				if (!(i % 3)) {
 					columns.push(<div className="clearfix visible-md-block" />);
+					if (!(i % 6)) {
+						columns.push(
+							<div className="visible-md">
+								{getAd(i / 6 - 1)}
+							</div>,
+						);
+					}
 				}
 				if (!(i % 4)) {
 					columns.push(<div className="clearfix visible-lg-block" />);
+					if (!(i % 8)) {
+						columns.push(
+							<div className="visible-lg">
+								{getAd(i / 8 - 1)}
+							</div>,
+						);
+					}
 				}
 			}
 
