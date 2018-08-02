bf4f5bc14469c9b35b72432e0961ca763a6d4a04
Benedict Etzel <benedict@hearthsim.net>
2018-08-03 00:20:08 +0200

    Add Facebook Pixel code behind a feature flag

--- hsreplaynet/templates/footerjs.js
+++ hsreplaynet/templates/footerjs.js
@@ -1,5 +1,6 @@
 {% load web_extras %}
 {% load get_current_language from i18n %}
+{% load feature from features %}
 {% if request.dnt %}
 // You are sending DNT. We won't serve you Analytics.
 {% else %}
@@ -25,6 +26,20 @@ ga("send", "pageview", {
 	page: location.pathname,
 });
 ga("set", "dimension1", "{% get_current_language as lang %}{{ lang }}");
+{% endif %}
+{% feature "facebook-pixel" as pixelft %}
+{% setting "FACEBOOK_PIXEL" as fbpixel %}
+{% if pixelft.enabled and fbpixel and not premium %}
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window,document,"script",
+	"https://connect.facebook.net/en_US/fbevents.js");
+fbq("init", "{{ fbpixel }}");
+fbq("track", "PageView");
 {% endif %}
 
 {% endif %}
