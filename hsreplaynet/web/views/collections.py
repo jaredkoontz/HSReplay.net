f4f0e2db5027de621ed1004e5f3a97af7d4625d5
Jerome Leclanche <jerome@leclan.ch>
2018-08-16 15:23:52 +0300

    Change collection to a better title

--- hsreplaynet/web/views/collections.py
+++ hsreplaynet/web/views/collections.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
 
 from hearthsim.identity.accounts.models import BlizzardAccount
@@ -29,11 +30,17 @@ class CollectionView(SimpleReactView):
 		lo = self.kwargs.get("lo", 0)
 		try:
 			account = BlizzardAccount.objects.get(region=region, account_lo=lo)
-		except Exception:
+		except BlizzardAccount.DoesNotExist:
 			return {}
+
+		self.request.head.title = format_lazy(
+			_("{user}'s Hearthstone Collection"),
+			user=str(account.battletag)
+		)
+
 		return {
 			"region": region,
 			"account_lo": lo,
 			"owner": False,
-			"battletag": account.battletag if account else "Unknown"
+			"battletag": account.battletag,
 		}
