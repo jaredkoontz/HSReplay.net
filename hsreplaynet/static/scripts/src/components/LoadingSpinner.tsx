34f5a77d20273cc15bbc6a2eab9bef5b156929dc
Alexander Zeier <alex@hearthsim.net>
2018-09-13 03:36:28 -0700

    Implement a 30 second timeout on the LoadingSpinner

--- hsreplaynet/static/scripts/src/components/LoadingSpinner.tsx
+++ hsreplaynet/static/scripts/src/components/LoadingSpinner.tsx
@@ -1,19 +1,74 @@
 import React from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
 
-interface Props {
+interface Props extends InjectedTranslateProps {
 	active?: boolean;
 	small?: boolean;
 }
 
-export default class LoadingSpinner extends React.Component<Props> {
+interface State {
+	timedOut: boolean;
+}
+
+class LoadingSpinner extends React.Component<Props, State> {
+	timeout: number | null = null;
+
+	constructor(props: Props, context: any) {
+		super(props, context);
+		this.state = {
+			timedOut: false,
+		};
+	}
+
+	componentDidMount() {
+		if (this.props.active) {
+			this.timeout = window.setTimeout(() => {
+				this.setState({ timedOut: true });
+			}, 30000);
+		}
+	}
+
+	componentWillUnmount() {
+		if (this.timeout !== null) {
+			window.clearTimeout(this.timeout);
+		}
+	}
+
+	componentDidUpdate(prevProps: Props) {
+		if (prevProps.active !== this.props.active) {
+			this.setState({ timedOut: false });
+			if (this.timeout !== null) {
+				window.clearTimeout(this.timeout);
+				this.timeout = null;
+			}
+			if (this.props.active) {
+				this.timeout = window.setTimeout(() => {
+					this.setState({ timedOut: true });
+				}, 30000);
+			}
+		}
+	}
+
 	public render(): React.ReactNode {
-		if (!this.props.active) {
+		const { active, t } = this.props;
+
+		if (!active) {
 			return null;
 		}
+
+		if (this.state.timedOut) {
+			return (
+				<h3 className="message-wrapper">
+					{t("Please check back later")}
+				</h3>
+			);
+		}
+
 		const className = ["loading-spinner"];
 		if (this.props.small) {
 			className.push("small");
 		}
+
 		return (
 			<div className={className.join(" ")}>
 				{Array.apply(null, { length: 12 }).map((x, i) => (
@@ -23,3 +78,5 @@ export default class LoadingSpinner extends React.Component<Props> {
 		);
 	}
 }
+
+export default translate()(LoadingSpinner);
