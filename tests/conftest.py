a25dc234b1e4f91d88ea9d21fd71da643c80bdb8
Benedict Etzel <benedict@hearthsim.net>
2018-09-24 12:28:40 +0200

    Add api_key and auth_token fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import os
 import subprocess
+from uuid import uuid4
 
 import pytest
+
+from hearthsim.identity.accounts.models import AuthToken
+from hearthsim.identity.api.models import APIKey
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -23,3 +27,25 @@ def user(db):
 	from django.contrib.auth import get_user_model
 	user, created = get_user_model().objects.get_or_create(username="user")
 	return user
+
+
+@pytest.yield_fixture(scope="function")
+def api_key(db):
+	return APIKey.objects.get_or_create(
+		api_key=str(uuid4()),
+		defaults={
+			"full_name": "Test Client",
+			"email": "test@example.org",
+			"website": "https://example.org",
+		}
+	)[0]
+
+
+@pytest.yield_fixture(scope="function")
+def auth_token(db, api_key):
+	token = AuthToken.objects.get_or_create(
+		key=str(uuid4()),
+		creation_apikey=api_key
+	)[0]
+	token.create_fake_user(save=True)
+	return token
