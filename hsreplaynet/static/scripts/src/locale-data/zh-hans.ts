0aa51a4fb82f529db7536db908754a63d6afca79
Benedict Etzel <benedict@hearthsim.net>
2018-09-25 11:45:59 +0200

    Fix wrong number locale for zh-hans

--- hsreplaynet/static/scripts/src/locale-data/zh-hans.ts
+++ hsreplaynet/static/scripts/src/locale-data/zh-hans.ts
@@ -1,5 +1,5 @@
 import icu from "i18next-icu/locale-data/zh";
 import dateFns from "date-fns/locale/zh_cn";
-import numbro from "numbro/languages/ru-RU";
+import numbro from "numbro/languages/zh-CN";
 
 export { icu, dateFns, numbro };
