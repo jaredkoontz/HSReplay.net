8521a27905cda85edfc6ca3a982ab326bed3a4d2
Benedict Etzel <benedict@hearthsim.net>
2018-10-02 14:36:54 +0200

    Enable React's StrictMode
    React's StrictMode enforces various best practives and shows deprecation
    notices during development[1].
    
    [1]: https://reactjs.org/docs/strict-mode.html

--- hsreplaynet/static/scripts/src/components/Root.tsx
+++ hsreplaynet/static/scripts/src/components/Root.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ErrorReporter from "./ErrorReporter";
 import { Provider as HearthstoneAccountProvider } from "./utils/hearthstone-account";
 import { I18nextProvider } from "react-i18next";
@@ -12,13 +12,18 @@ AdHelper.create();
 export default class Root extends React.Component {
 	public render(): React.ReactNode {
 		return (
-			<I18nextProvider i18n={i18n} initialLanguage={UserData.getLocale()}>
-				<ErrorReporter>
-					<HearthstoneAccountProvider>
-						{this.props.children}
-					</HearthstoneAccountProvider>
-				</ErrorReporter>
-			</I18nextProvider>
+			<StrictMode>
+				<I18nextProvider
+					i18n={i18n}
+					initialLanguage={UserData.getLocale()}
+				>
+					<ErrorReporter>
+						<HearthstoneAccountProvider>
+							{this.props.children}
+						</HearthstoneAccountProvider>
+					</ErrorReporter>
+				</I18nextProvider>
+			</StrictMode>
 		);
 	}
 }
