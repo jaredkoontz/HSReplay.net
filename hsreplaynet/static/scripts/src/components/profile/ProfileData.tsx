01a23e623de5650c62ff8ed18e0c7551d905847a
Alexander Zeier <alex@hearthsim.net>
2018-10-18 15:07:05 -0700

    Add matchup matrix to Profile page

---
+++ hsreplaynet/static/scripts/src/components/profile/ProfileData.tsx
@@ -0,0 +1,253 @@
+import React from "react";
+import DataInjector from "../DataInjector";
+import { BnetGameType, CardClass, FormatType } from "../../hearthstone";
+import { Archetype } from "../../utils/api";
+import { ArchetypeData } from "../../interfaces";
+
+export type ProfileDataType = "MatchupData";
+
+interface Props {
+	userId: number;
+	type: ProfileDataType;
+	replayFilter?: (replay: ReplayData) => boolean;
+}
+
+export interface ReplayData {
+	user_id: number;
+	match_start: string;
+	match_end: string;
+	shortid: string | null;
+	digest: string | null;
+	game_type: BnetGameType;
+	format_type: FormatType;
+	ladder_season: number;
+	brawl_season: number;
+	scenario_id: number;
+	num_turns: number;
+	friendly_player_account_hi: string;
+	friendly_player_account_lo: string;
+	friendly_player_battletag: string;
+	friendly_player_is_first: boolean;
+	friendly_player_rank: number | null;
+	friendly_player_legend_rank: number | null;
+	friendly_player_rank_stars: number | null;
+	friendly_player_wins: number | null;
+	friendly_player_losses: number | null;
+	friendly_player_class: CardClass;
+	friendly_player_hero: number;
+	friendly_player_deck: string;
+	friendly_player_blizzard_deck_id: number | null;
+	friendly_player_cardback_id: number | null;
+	friendly_player_final_state: number;
+	friendly_player_archetype_id: number | null;
+	opponent_account_hi: string;
+	opponent_account_lo: string;
+	opponent_battletag: string;
+	opponent_is_ai: boolean;
+	opponent_rank: number | null;
+	opponent_legend_rank: number | null;
+	opponent_class: CardClass;
+	opponent_hero: number;
+	opponent_revealed_deck: string;
+	opponent_predicted_deck: string;
+	opponent_cardback_id: number | null;
+	opponent_final_state: number;
+	opponent_archetype_id: number | null;
+	replay_xml: string;
+	hslog_version: string;
+	disconnected: boolean;
+	reconnecting: boolean;
+	visibility: number;
+	views: number;
+}
+
+export default class ProfileData extends React.Component<Props> {
+	public render(): React.ReactNode {
+		return (
+			<DataInjector
+				query={[
+					{
+						key: "replays",
+						params: { user_id: this.props.userId },
+						url: "/api/v1/replays",
+					},
+					{
+						key: "archetypeData",
+						params: {},
+						url: "/api/v1/archetypes",
+					},
+					{
+						key: "matchupData",
+						params: {}, // TODO: add params
+						url: "head_to_head_archetype_matchups",
+					},
+				]}
+			>
+				{({ replays, archetypeData, matchupData }) => {
+					return (this.props.children as any)(
+						this.transformData(replays, archetypeData, matchupData),
+					);
+				}}
+			</DataInjector>
+		);
+	}
+
+	private transformData(
+		replays: ReplayData[],
+		archetypeData: Archetype[],
+		globalMatchupData: any,
+	): any {
+		if (!replays || !archetypeData || !globalMatchupData) {
+			return null;
+		}
+		const filteredReplays = this.props.replayFilter
+			? replays.filter(this.props.replayFilter)
+			: replays;
+		switch (this.props.type) {
+			case "MatchupData":
+				return this.getMatchupData(
+					filteredReplays,
+					archetypeData,
+					globalMatchupData,
+				);
+		}
+		return null;
+	}
+
+	private getMatchupData(
+		replays: ReplayData[],
+		archetypeData: Archetype[],
+		globalMatchupData: any,
+	): any {
+		const rawMatchups = {};
+		const incrementMatchup = (
+			friendlyId: number,
+			opposingId: number,
+			fFinalState: number,
+		) => {
+			if (!rawMatchups[friendlyId]) {
+				rawMatchups[friendlyId] = {};
+			}
+			if (!rawMatchups[friendlyId][opposingId]) {
+				rawMatchups[friendlyId][opposingId] = { wins: 0, games: 0 };
+			}
+			rawMatchups[friendlyId][opposingId].games += 1;
+			if (fFinalState === 4) {
+				rawMatchups[friendlyId][opposingId].wins += 1;
+			}
+		};
+
+		const friendlyArchetypeCounts = {};
+		const opposingArchetypeCounts = {};
+		const archetypeIds = new Set<number>();
+		const incrementArchetypeCount = (
+			friendlyId: number,
+			opposingId: number,
+		) => {
+			archetypeIds.add(friendlyId);
+			archetypeIds.add(opposingId);
+			friendlyArchetypeCounts[friendlyId] =
+				(friendlyArchetypeCounts[friendlyId] || 0) + 1;
+			opposingArchetypeCounts[opposingId] =
+				(opposingArchetypeCounts[opposingId] || 0) + 1;
+		};
+
+		replays.forEach(replay => {
+			const friendlyId = replay.friendly_player_archetype_id;
+			const opposingId = replay.opponent_archetype_id;
+			if (friendlyId && opposingId) {
+				incrementArchetypeCount(friendlyId, opposingId);
+				incrementMatchup(
+					friendlyId,
+					opposingId,
+					replay.friendly_player_final_state,
+				);
+			}
+		});
+
+		const toRemove = Object.keys(opposingArchetypeCounts).map(Number);
+		const matchups: ArchetypeData[] = [];
+		archetypeIds.forEach(friendlyId => {
+			const friendlyArchetype = archetypeData.find(
+				a => a.id === +friendlyId,
+			);
+			if (!friendlyArchetype) {
+				return;
+			}
+			const innerMatchups = [];
+			archetypeIds.forEach(opposingId => {
+				const opposingArchetype = archetypeData.find(
+					a => a.id === +opposingId,
+				);
+				if (!opposingArchetype) {
+					return;
+				}
+				const data =
+					rawMatchups[friendlyId] &&
+					rawMatchups[friendlyId][opposingId];
+				if (data && data.games) {
+					const index = toRemove.indexOf(opposingId);
+					if (index > -1) {
+						toRemove.splice(index, 1);
+					}
+				}
+				const globalFriendly =
+					globalMatchupData.series.data["" + friendlyId];
+				const globalOpposing =
+					globalFriendly && globalFriendly["" + opposingId];
+				const globalWinrate =
+					globalOpposing && globalOpposing.total_games > 30
+						? globalOpposing.win_rate
+						: undefined;
+				innerMatchups.push({
+					friendlyId: +friendlyId,
+					friendlyName: friendlyArchetype.name,
+					friendlyPlayerClass: friendlyArchetype.player_class_name,
+					opponentId: +opposingId,
+					opponentName: opposingArchetype.name,
+					opponentPlayerClass: opposingArchetype.player_class_name,
+					winrate: data ? data.wins / data.games * 100 : 50,
+					totalGames: data ? data.games : 0,
+					globalWinrate,
+				});
+			});
+			if (innerMatchups.some(x => x.totalGames)) {
+				matchups.push({
+					id: +friendlyId,
+					name: friendlyArchetype.name,
+					playerClass: friendlyArchetype.player_class_name,
+					popularityTotal: 0,
+					popularityClass: 0,
+					winrate: 50,
+					effectiveWinrate: 50,
+					matchups: innerMatchups,
+				});
+			} else {
+				delete friendlyArchetypeCounts[friendlyId];
+			}
+		});
+		if (toRemove.length) {
+			matchups.forEach(matchup => {
+				matchup.matchups = matchup.matchups.filter(
+					x => toRemove.indexOf(x.opponentId) === -1,
+				);
+			});
+			toRemove.forEach(id => {
+				delete opposingArchetypeCounts[id];
+			});
+		}
+		const friendlyArchetypeIds = Object.keys(friendlyArchetypeCounts).map(
+			Number,
+		);
+		friendlyArchetypeIds.sort(
+			(a, b) => friendlyArchetypeCounts[b] - friendlyArchetypeCounts[a],
+		);
+		const opposingArchetypeIds = Object.keys(opposingArchetypeCounts).map(
+			Number,
+		);
+		opposingArchetypeIds.sort(
+			(a, b) => opposingArchetypeCounts[b] - opposingArchetypeCounts[a],
+		);
+		return { matchups, friendlyArchetypeIds, opposingArchetypeIds };
+	}
+}
--- hsreplaynet/static/scripts/src/pages/Profile.tsx
+++ hsreplaynet/static/scripts/src/pages/Profile.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import scrollbarSize from "dom-helpers/util/scrollbarSize";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import CardData from "../CardData";
 import { Archetype } from "../utils/api";
@@ -6,6 +7,9 @@ import RankIcon from "../components/RankIcon";
 import ProfileHero from "../components/profile/ProfileHero";
 import ProfileHighlight from "../components/profile/ProfileHighlight";
 import { BnetGameType } from "../hearthstone";
+import ProfileData, { ReplayData } from "../components/profile/ProfileData";
+import LoadingSpinner from "../components/LoadingSpinner";
+import ArchetypeMatrix from "../components/metaoverview/matchups/ArchetypeMatrix";
 import Tab from "../components/layout/Tab";
 import TabList from "../components/layout/TabList";
 
@@ -46,6 +50,7 @@ class Profile extends React.Component<Props, State> {
 							{/*TODO: Dropdown*/}
 						</h1>
 						<h3>{t("Archetype Matchups")}</h3>
+						{this.renderMatchupMatrix()}
 					</Tab>
 					<Tab id={"RANKED_WILD"} label={"Ranked Wild"} disabled />
 					<Tab id={"ARENA"} label={"Arena"} disabled />
@@ -64,6 +69,100 @@ class Profile extends React.Component<Props, State> {
 		}
 		return null;
 	}
+
+	private isValidTimeFrame(replay: ReplayData): boolean {
+		const timeFrameStart = this.getTimeFrameStart();
+		if (timeFrameStart === null) {
+			return false;
+		}
+		const matchStart = Date.parse(replay.match_start);
+		return matchStart >= timeFrameStart.getTime();
+	}
+
+	private renderMatchupMatrix(): React.ReactNode {
+		const { t } = this.props;
+		const cellWidth = 90;
+		const cellHeight = 50;
+		return (
+			<ProfileData
+				userId={this.props.userId}
+				type="MatchupData"
+				replayFilter={replay =>
+					replay.game_type === BnetGameType.BGT_RANKED_STANDARD &&
+					this.isValidTimeFrame(replay)
+				}
+			>
+				{data => {
+					if (
+						!this.props.archetypeData ||
+						!this.props.cardData ||
+						!data
+					) {
+						return (
+							<div
+								className="archetype-matrix-wrapper"
+								style={{ height: 200, width: 200 }}
+							>
+								<LoadingSpinner active />
+							</div>
+						);
+					}
+					if (
+						!data.friendlyArchetypeIds.length ||
+						!data.opposingArchetypeIds.length
+					) {
+						return (
+							<div
+								className="archetype-matrix-wrapper"
+								style={{ height: 200 }}
+							>
+								<h3>{t("No data available")}</h3>
+							</div>
+						);
+					}
+					return (
+						<div
+							className="archetype-matrix-wrapper"
+							style={{
+								height:
+									100 +
+									scrollbarSize() +
+									data.friendlyArchetypeIds.length *
+										cellHeight +
+									5,
+							}}
+						>
+							<ArchetypeMatrix
+								archetypeMatchups={data.matchups}
+								friendlyArchetypes={data.friendlyArchetypeIds}
+								opposingArchetypes={data.opposingArchetypeIds}
+								allArchetypes={this.props.archetypeData}
+								cardData={this.props.cardData}
+								customWeights={{}}
+								onCustomWeightsChanged={() => null}
+								useCustomWeights={false}
+								onUseCustomWeightsChanged={() => null}
+								favorites={[]}
+								gameType="RANKED_STANDARD"
+								ignoredColumns={[]}
+								onFavoriteChanged={() => null}
+								onIgnoreChanged={() => null}
+								onSortChanged={() => null}
+								sortBy="name"
+								sortDirection="ascending"
+								simple
+								minGames={1}
+								ignoreMirror
+								columnHeaderLinks
+								cellWidth={cellWidth}
+								cellHeight={cellHeight}
+								cellColorStyle={"text"}
+							/>
+						</div>
+					);
+				}}
+			</ProfileData>
+		);
 	}
 
 	private renderHeader(): React.ReactNode {
