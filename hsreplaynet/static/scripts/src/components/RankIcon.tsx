0409750a5679a851482e10af0d79b98b5135ad5c
Benedict Etzel <benedict@hearthsim.net>
2018-10-19 03:06:47 +0200

    RankIcon: Add tooltip prop

--- hsreplaynet/static/scripts/src/components/RankIcon.tsx
+++ hsreplaynet/static/scripts/src/components/RankIcon.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { BnetGameType } from "../hearthstone";
 import { image } from "../helpers";
+import Tooltip from "./Tooltip";
+import { InjectedTranslateProps, translate } from "react-i18next";
 
-interface Props {
+interface Props extends InjectedTranslateProps {
 	gameType?: BnetGameType;
 	rank?: number;
 	legendRank?: number;
+	tooltip?: boolean;
 }
 
-export default class RankIcon extends React.Component<Props> {
+class RankIcon extends React.Component<Props> {
 	public render(): React.ReactNode {
+		const { rank, legendRank, t } = this.props;
 		let altText = null;
 		let imgPath = null;
 		if (
@@ -18,27 +22,35 @@ export default class RankIcon extends React.Component<Props> {
 				BnetGameType.BGT_RANKED_WILD,
 			].indexOf(this.props.gameType) !== -1
 		) {
-			if (this.props.rank) {
-				imgPath = `ranked-medals/Medal_Ranked_${this.props.rank}.png`;
-				altText = "Ranked";
+			if (rank) {
+				imgPath = `ranked-medals/Medal_Ranked_${rank}.png`;
+				altText = t("Rank {rank}", { rank });
 			}
-			if (this.props.legendRank) {
+			if (legendRank) {
 				imgPath = "ranked-medals/Medal_Ranked_Legend.png";
-				altText = `Legend ${+this.props.legendRank}`;
+				altText = t("Legend {rank}", { rank: legendRank });
 			}
 		} else {
 			return null;
 		}
 
-		return (
+		const result = (
 			<figure className="rank-icon-standalone">
 				<img src={image(imgPath)} alt={altText} />
-				<figcaption>
-					{this.props.legendRank
-						? this.props.legendRank
-						: this.props.rank}
-				</figcaption>
+				<figcaption>{legendRank ? legendRank : rank}</figcaption>
 			</figure>
 		);
+
+		if (this.props.tooltip) {
+			return (
+				<Tooltip content={altText} simple>
+					{result}
+				</Tooltip>
+			);
+		}
+
+		return result;
 	}
 }
+
+export default translate()(RankIcon);
