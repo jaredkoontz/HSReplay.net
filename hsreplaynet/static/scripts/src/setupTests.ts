6690f32c89d5520726039c1a589c0db0f50e5b53
Benedict Etzel <benedict@hearthsim.net>
2018-10-22 21:41:25 +0200

    Clean up setupTests.ts

--- hsreplaynet/static/scripts/src/setupTests.ts
+++ hsreplaynet/static/scripts/src/setupTests.ts
@@ -1,7 +1,7 @@
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "jest-enzyme";
-import fetch = require("jest-fetch-mock");
+import fetch from "jest-fetch-mock";
 
 Enzyme.configure({ adapter: new Adapter() });
 
