4070d55fa1837c0aa01355650b5e46d8b4b902be
Alexander Zeier <alex@hearthsim.net>
2018-12-02 11:30:12 -0800

    Disable MobilePromo if utm_souce is arcanetracker

--- hsreplaynet/static/scripts/src/components/MobilePromo.tsx
+++ hsreplaynet/static/scripts/src/components/MobilePromo.tsx
@@ -8,49 +8,67 @@ interface Props extends InjectedTranslateProps {
 	onClose: () => void;
 }
 
-const MobilePromo: React.SFC<Props> = ({ t, onClose }) => {
-	if (navigator.userAgent.toLowerCase().indexOf("android") === -1) {
-		return null;
+class MobilePromo extends React.Component<Props> {
+	private setting: string = "closed-mobile-promo-arcane-tracker";
+
+	constructor(props: Props, context?: any) {
+		super(props, context);
+		if (
+			!this.hasSetting() &&
+			window.location.search.indexOf("utm_source=arcanetracker") !== -1
+		) {
+			UserData.setSetting(this.setting, "1");
+		}
 	}
-	if (!UserData.hasFeature("arcane-tracker")) {
-		return null;
+
+	private hasSetting(): boolean {
+		return UserData.getSetting(this.setting) === "1";
 	}
-	const setting = "closed-mobile-promo-arcane-tracker";
-	if (UserData.getSetting(setting) === "1") {
-		return null;
+
+	render() {
+		const { t, onClose } = this.props;
+		if (navigator.userAgent.toLowerCase().indexOf("android") === -1) {
+			return null;
+		}
+		if (!UserData.hasFeature("arcane-tracker")) {
+			return null;
+		}
+		if (UserData.getSetting(this.setting) === "1") {
+			return null;
+		}
+		return (
+			<Panel
+				header={
+					<>
+						<span>{t("Don't you have a phone?")}</span>
+						<a
+							href="#"
+							className="btn mobile-promo-close"
+							onClick={e => {
+								e.preventDefault();
+								UserData.setSetting(this.setting, "1");
+								onClose();
+							}}
+						>
+							<span className="glyphicon glyphicon-remove" />
+						</a>
+					</>
+				}
+			>
+				<h2>
+					{t("Hearthstone Deck Tracker is now available on Android!")}
+				</h2>
+				<DownloadButton
+					id="Android-promo"
+					icon="android"
+					title={t("Download")}
+					subtitle="Arcane Tracker"
+					url="https://play.google.com/store/apps/details?id=net.mbonnin.arcanetracker"
+					target="_blank"
+				/>
+			</Panel>
+		);
 	}
-	return (
-		<Panel
-			header={
-				<>
-					<span>{t("Don't you have a phone?")}</span>
-					<a
-						href="#"
-						className="btn mobile-promo-close"
-						onClick={e => {
-							e.preventDefault();
-							UserData.setSetting(setting, "1");
-							onClose();
-						}}
-					>
-						<span className="glyphicon glyphicon-remove" />
-					</a>
-				</>
-			}
-		>
-			<h2>
-				{t("Hearthstone Deck Tracker is now available on Android!")}
-			</h2>
-			<DownloadButton
-				id="Android-promo"
-				icon="android"
-				title={t("Download")}
-				subtitle="Arcane Tracker"
-				url="https://play.google.com/store/apps/details?id=net.mbonnin.arcanetracker"
-				target="_blank"
-			/>
-		</Panel>
-	);
-};
+}
 
 export default translate()(MobilePromo);
