22cd0eb642ed80066552cec06c0dfb1df5226e3f
Benedict Etzel <benedict@hearthsim.net>
2019-01-09 13:37:54 +0100

    Upgrade i18next-scanner
    The new version no longer needs the React.Fragment rewrite as it can
    now cope with it (i18next/i18next-scanner#86) and additionally supports
    syntax that did not work before.

--- scripts/i18n_frontend_extract.sh
+++ scripts/i18n_frontend_extract.sh
@@ -4,7 +4,4 @@ rm -r build/generated/locale-extract
 
 # At this time, tsc throws some errors due to duplicate types. We don't care though, as it emits either way
 yarn run i18n:build
-
-find build/generated/locale-extract -type f -print0 | xargs -0 sed -i -r "s/<(\/?)>/<\1React.Fragment>/g"
-
 yarn run i18n:extract
