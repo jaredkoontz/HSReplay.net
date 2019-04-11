jaredkoontz/HSReplay.net

commit 91c73e8feb77a716eba4de0748269bc5209b476b

Tweak semantic markup on Decks and Cards pages

hsreplaynet/static/scripts/src/pages/Cards.tsx
@@ -266,16 +266,16 @@ class Cards extends React.Component<Props, State> {
 					uniqueId="cl-mlb1"
 					mobile
 				/>
-				<div className="cards">
-					<aside
+				<main className="cards">
+					<div
 						className={filterClassNames.join(" ")}
 						id="cards-infobox"
 					>
 						{backButton}
 						{this.renderFilters()}
 						{backButton}
-					</aside>
-					<main className={contentClassNames.join(" ")}>
+					</div>
+					<div className={contentClassNames.join(" ")}>
 						<Sticky top={10}>
 							<NetworkNAdUnit
 								id="nn_lb1"
@@ -304,8 +304,8 @@ class Cards extends React.Component<Props, State> {
 						</button>
 						{this.renderSearch()}
 						{this.renderContent()}
-					</main>
-				</div>
+					</div>
+				</main>
 			</CardFilterManager>
 		);
 	}
@@ -342,15 +342,7 @@ class Cards extends React.Component<Props, State> {
 		const isStatsView = this.isStatsView();
 		const { t } = this.props;
 
-		const filters = [
-			<ResetHeader
-				key="reset"
-				onReset={() => this.resetFilters()}
-				showReset={showReset}
-			>
-				{isStatsView ? t("Cards") : t("Gallery")}
-			</ResetHeader>,
-		];
+		const filters = [];
 
 		const modeFilter = (
 			<section id="mode-filter" key="mode-filter">
@@ -639,7 +631,18 @@ class Cards extends React.Component<Props, State> {
 			</>,
 		);
 
-		return filters;
+		return (
+			<>
+				<ResetHeader
+					key="reset"
+					onReset={() => this.resetFilters()}
+					showReset={showReset}
+				>
+					{isStatsView ? t("Cards") : t("Gallery")}
+				</ResetHeader>
+				<aside>{filters}</aside>
+			</>
+		);
 	}
 
 	renderSearch(): React.ReactNode {

hsreplaynet/static/scripts/src/components/DeckList.tsx
@@ -320,7 +320,8 @@ class DeckList extends React.Component<Props, State> {
 		}
 
 		return (
-			<div className="deck-list">
+			<section className="deck-list">
+				<h2 className="sr-only">{t("List of decks")}</h2>
 				{this.props.helpMessage ? (
 					<p className="help-block pull-left">
 						<span className="visible-sm-inline">&nbsp;</span>
@@ -421,7 +422,7 @@ class DeckList extends React.Component<Props, State> {
 					{deckTiles}
 				</ul>
 				{pager(false)}
-			</div>
+			</section>
 		);
 	}
 }

hsreplaynet/static/scripts/src/components/DeckTile.tsx
@@ -266,7 +266,7 @@ class DeckTile extends React.Component<Props> {
 						data-card-class={this.props.playerClass}
 					>
 						<div className="col-lg-2 col-md-2 col-sm-2 col-xs-6">
-							<span
+							<h3
 								className="deck-name"
 								style={{
 									backgroundImage:
@@ -278,7 +278,7 @@ class DeckTile extends React.Component<Props> {
 								}}
 							>
 								{deckName}
-							</span>
+							</h3>
 							<small>{headerData}</small>
 							{globalDataIndicator}
 						</div>

hsreplaynet/static/scripts/src/components/Tooltip.tsx
@@ -165,12 +165,12 @@ export default class Tooltip extends React.Component<Props, State> {
 				onTouchStart={() => this.setState({ isTouchDevice: true })}
 			>
 				{!this.props.noSrTooltip ? (
-					<section className="sr-only">
+					<aside className="sr-only">
 						{this.props.header ? (
 							<h4>{this.props.header}</h4>
 						) : null}
 						{content}
-					</section>
+					</aside>
 				) : null}
 				{this.renderTooltip()}
 				{this.props.children}

hsreplaynet/static/scripts/src/components/__tests__/__snapshots__/TwitchVodsTable.spec.tsx.snap
@@ -497,13 +497,13 @@ exports[`TwitchVodsTable allows selecting a VOD: brandonsmithx01 1`] = `
                           onMouseMove={[Function]}
                           onTouchStart={[Function]}
                         >
-                          <section
+                          <aside
                             className="sr-only"
                           >
                             <p>
                               Legend 1555
                             </p>
-                          </section>
+                          </aside>
                           <figure
                             className="rank-icon-standalone"
                           >
@@ -654,13 +654,13 @@ exports[`TwitchVodsTable allows selecting a VOD: brandonsmithx01 1`] = `
                           onMouseMove={[Function]}
                           onTouchStart={[Function]}
                         >
-                          <section
+                          <aside
                             className="sr-only"
                           >
                             <p>
                               Rank 1
                             </p>
-                          </section>
+                          </aside>
                           <figure
                             className="rank-icon-standalone"
                           >
@@ -716,7 +716,7 @@ exports[`TwitchVodsTable allows selecting a VOD: brandonsmithx01 1`] = `
                       onMouseMove={[Function]}
                       onTouchStart={[Function]}
                     >
-                      <section
+                      <aside
                         className="sr-only"
                       >
                         <Component
@@ -734,7 +734,7 @@ exports[`TwitchVodsTable allows selecting a VOD: brandonsmithx01 1`] = `
                             Priest
                           </PrettyCardClass>
                         </Component>
-                      </section>
+                      </aside>
                       <img
                         className="class-icon"
                         src="/static/images/64x/class-icons/priest.png"
@@ -1298,13 +1298,13 @@ exports[`TwitchVodsTable renders correctly: default 1`] = `
                           onMouseMove={[Function]}
                           onTouchStart={[Function]}
                         >
-                          <section
+                          <aside
                             className="sr-only"
                           >
                             <p>
                               Legend 1555
                             </p>
-                          </section>
+                          </aside>
                           <figure
                             className="rank-icon-standalone"
                           >
@@ -1455,13 +1455,13 @@ exports[`TwitchVodsTable renders correctly: default 1`] = `
                           onMouseMove={[Function]}
                           onTouchStart={[Function]}
                         >
-                          <section
+                          <aside
                             className="sr-only"
                           >
                             <p>
                               Rank 1
                             </p>
-                          </section>
+                          </aside>
                           <figure
                             className="rank-icon-standalone"
                           >
@@ -1517,7 +1517,7 @@ exports[`TwitchVodsTable renders correctly: default 1`] = `
                       onMouseMove={[Function]}
                       onTouchStart={[Function]}
                     >
-                      <section
+                      <aside
                         className="sr-only"
                       >
                         <Component
@@ -1535,7 +1535,7 @@ exports[`TwitchVodsTable renders correctly: default 1`] = `
                             Priest
                           </PrettyCardClass>
                         </Component>
-                      </section>
+                      </aside>
                       <img
                         className="class-icon"
                         src="/static/images/64x/class-icons/priest.png"

hsreplaynet/static/scripts/src/components/__tests__/__snapshots__/TwitchVodsTableItem.spec.tsx.snap
@@ -11,13 +11,13 @@ Array [
       onMouseMove={[Function]}
       onTouchStart={[Function]}
     >
-      <section
+      <aside
         className="sr-only"
       >
         <p>
           Legend 1234
         </p>
-      </section>
+      </aside>
       <figure
         className="rank-icon-standalone"
       >
@@ -52,11 +52,11 @@ Array [
       onMouseMove={[Function]}
       onTouchStart={[Function]}
     >
-      <section
+      <aside
         className="sr-only"
       >
         Mage
-      </section>
+      </aside>
       <img
         className="class-icon"
         src="/static/images/64x/class-icons/mage.png"

hsreplaynet/static/scripts/src/pages/Decks.tsx
@@ -680,7 +680,7 @@ class Decks extends React.Component<Props, State> {
 		const premiumTabIndex = isPremium ? 0 : -1;
 
 		return (
-			<div className="decks">
+			<main className="decks">
 				<div className={filterClassNames.join(" ")} id="decks-infobox">
 					{backButton}
 					<ResetHeader
@@ -1337,7 +1337,7 @@ class Decks extends React.Component<Props, State> {
 						<NetworkNAdUnit id="nn_mpu3" uniqueId="dl-mpu1" />
 					</aside>
 				</div>
-				<main
+				<div
 					className={contentClassNames.join(" ")}
 					ref={ref => (this.mainRef = ref)}
 				>
@@ -1365,8 +1365,8 @@ class Decks extends React.Component<Props, State> {
 						mobile
 						center
 					/>
-				</main>
-			</div>
+				</div>
+			</main>
 		);
 	}
 

hsreplaynet/static/styles/cards.scss
@@ -98,7 +98,7 @@
 		}
 	}
 
-	> main {
+	.card-list-wrapper {
 		min-height: 100vh;
 		padding: 10px;
 		width: calc(100% - 300px);

commit 25ce4d83390f7d2cfb71b68e085d8bbff781471f

Fix a sticky ad on Cards page
Regression from 91c73e8feb77a716eba4de0748269bc5209b476b.

hsreplaynet/static/scripts/src/pages/Cards.tsx
@@ -621,16 +621,6 @@ class Cards extends React.Component<Props, State> {
 				</InfoboxFilterGroup>,
 			);
 		}
-
-		filters.push(
-			<>
-				<NetworkNAdUnit id="nn_mpu1" uniqueId="cl-mpu1" />
-				<Sticky bottom={0} key="networkn-ad">
-					<NetworkNAdUnit id="nn_mpu2" uniqueId="cl-mpu2" />
-				</Sticky>
-			</>,
-		);
-
 		return (
 			<>
 				<ResetHeader
@@ -641,6 +631,10 @@ class Cards extends React.Component<Props, State> {
 					{isStatsView ? t("Cards") : t("Gallery")}
 				</ResetHeader>
 				<aside>{filters}</aside>
+				<NetworkNAdUnit id="nn_mpu1" uniqueId="cl-mpu1" />
+				<Sticky bottom={0} key="networkn-ad">
+					<NetworkNAdUnit id="nn_mpu2" uniqueId="cl-mpu2" />
+				</Sticky>
 			</>
 		);
 	}