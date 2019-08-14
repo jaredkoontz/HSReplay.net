1d654ac08c0cfc91197f0bf988a2cefe1bc3624a
Benedict Etzel <benedict@hearthsim.net>
2019-08-14 14:49:31 +0200

    Rename Ritual filter to C'Thun

--- hsreplaynet/static/scripts/src/components/cards/filters/MechanicsFilter.tsx
+++ hsreplaynet/static/scripts/src/components/cards/filters/MechanicsFilter.tsx
@@ -69,7 +69,7 @@ class MechanicsFilter extends React.Component<Props> {
 				<CardFilterItem value={"DISCOVER"}>
 					{t("GLOBAL_KEYWORD_DISCOVER")}
 				</CardFilterItem>
-				<CardFilterItem value={"RITUAL"}>{t("Ritual")}</CardFilterItem>
+				<CardFilterItem value={"RITUAL"}>{t("C'Thun")}</CardFilterItem>
 				<CardFilterItem value={"JADE_GOLEM"}>
 					{t("GLOBAL_KEYWORD_JADE_GOLEM")}
 				</CardFilterItem>
